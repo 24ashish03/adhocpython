68fa70176db34189e2807d39b1bfb29bbb97c48c
24ashish03 <42356121+24ashish03@users.noreply.github.com>
2018-09-12 20:07:07 +0530

    Add files via upload

---
+++ add1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env  python2
+
+import  sys,time
+#  taking all numbers to add 
+user_data=sys.argv[1:]
+sum=0
+for   i   in  user_data:
+	sum=sum+int(i)
+
+
+print "addition of given numbers is:  ",sum
